0c843a1e4d4ca17a690109209394ed9d5c48dc12
Jack Hou <jackhou1998@gmail.com>
2018-03-18 18:41:36 -0700

    mostly functional keypad

--- G14_B_P2/src/LCD/keypad.py
+++ G14_B_P2/src/LCD/keypad.py
@@ -1,44 +1,80 @@
 from tkinter import *
 from tkinter import ttk
+import time
 
 root = Tk()
 
+#passcode is stored in _strSecret
+_strSecret = "1809"
+_strVar = ""
+
 def put_num1(event):
-    userEntry.insert(0,1)
+    userEntry.insert("end",1)
+    global _strVar
+    _strVar = _strVar + '1'
 
 def put_num2(event):
-    userEntry.insert(0,2)
+    userEntry.insert("end",2)
+    global _strVar
+    _strVar = _strVar +'2'
 
 def put_num3(event):
-    userEntry.insert(0,3)
+    userEntry.insert("end",3)
+    global _strVar
+    _strVar = _strVar +'3'
 
 def put_num4(event):
-    userEntry.insert(0,4)
+    userEntry.insert("end",4)
+    global _strVar
+    _strVar = _strVar +'4'
 
 def put_num5(event):
-    userEntry.insert(0,5)
+    userEntry.insert("end",5)
+    global _strVar
+    _strVar = _strVar +'5'
 
 def put_num6(event):
-    userEntry.insert(0,6)
+    userEntry.insert("end",6)
+    global _strVar
+    _strVar = _strVar +'6'
 
 def put_num7(event):
-    userEntry.insert(0,7)
+    userEntry.insert("end",7)
+    global _strVar
+    _strVar = _strVar + '7'
 
 def put_num8(event):
-    userEntry.insert(0,8)
+    userEntry.insert("end",8)
+    global _strVar
+    _strVar = _strVar + '8'
 
 def put_num9(event):
-    userEntry.insert(0,9)
+    userEntry.insert("end",9)
+    global _strVar
+    _strVar = _strVar + '9'
 
 def put_num0(event):
-    userEntry.insert(0,0)
+    userEntry.insert("end",0)
+    global _strVar
+    _strVar = _strVar + '0'  
 
 def clear_num(event):
     userEntry.delete(0,"end")
+    global _strVar
+    _strVar = "" #reset the _strVar 
 
 def enter_code(event):
-    userEntry.insert(0,"ENTERED")
+    userEntry.insert("end","ENTERED")
+    global _strVar
+    global _strSecret
+    print("Code entered: ", _strVar)
+    if _strVar == _strSecret:
+        print("Door is unlocked")
+    else:
+        print("Error: Incorrect code entered")
+    clear_num(NONE)
 
+root.geometry("120x120+20+20")
 userEntry = Entry(root,width=20)
 userEntry.grid(row = 0, columnspan = 7)
 
