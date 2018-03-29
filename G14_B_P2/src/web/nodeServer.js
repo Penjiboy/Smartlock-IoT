2a716727f48b2e6581060c431e7f00bed1e2f4d5
ali <aliumar18@gmail.com>
2018-03-29 00:01:07 -0700

    sockets

--- G14_B_P2/src/web/nodeServer.js
+++ G14_B_P2/src/web/nodeServer.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var io = require('socket.io')(http);
 var tj = require('templatesjs');
 
 http.createServer(handler).listen(80, "0.0.0.0");
@@ -26,3 +27,20 @@ function handler(req, res) {
         res.end()
     }
 }
+
+io.on('connection', function (socket) {
+
+total++;
+console.log(total, 'sockets connected')
+
+  socket.on('unlock', function () {
+    console.log("completed");
+    socket.emit("bbb");
+  });
+
+    socket.on('disconnect',function(){
+console.log('disconnected')
+    total--;
+    });
+
+});
