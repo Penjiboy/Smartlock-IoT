327cf187d277cc564587d47c3897b0d38a327247
penjiboy <chavulapenjani@gmail.com>
2018-03-29 09:54:13 -0700

    API request

---
+++ G14_B_P2/src/web/requests.js
@@ -0,0 +1,15 @@
+const https = require("http");
+const url =
+  "http://38.88.74.79:9014/todos";
+https.get(url, res => {
+  res.setEncoding("utf8");
+  let body = "";
+  res.on("data", data => {
+    body += data;
+  });
+  res.on("end", () => {
+    body = JSON.parse(body);
+    console.log(
+      body  );
+  });
+});
