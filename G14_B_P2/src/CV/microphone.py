3da955266acce6282acf16b1d8a851772eb086ab
Alexlee214 <31544596+Alexlee214@users.noreply.github.com>
2018-04-02 22:10:44 -0700

    the microphone now sending audio file through sftp

--- G14_B_P2/src/CV/microphone.py
+++ G14_B_P2/src/CV/microphone.py
@@ -3,8 +3,10 @@ import subprocess
 import time
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
+import pysftp
 
 fileName = "myMessage.wav"
+sftp =  pysftp.Connection('38.88.74.79', username='root', password='291#jHjRed26H')
 
 class micRecord:
     def recordOn(self):
@@ -16,7 +18,12 @@ class micRecord:
         subprocess.call(["pkill", "-f", "arecord"])
         print("message recorded and saved")
         time.sleep(0.5)
-        os.system("./sendRecording.sh")
+#        os.system("./sendRecording.sh")
+
+        sftp.chdir("G14_B_P2/G14_B_P2/src/web/mic/lock1")
+        sftp.put("myMessage.wav")
+        
+        
         print("recording sent over to the remote server")
 
 
